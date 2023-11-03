7eb22d3250b1f4ea27c83c15b40a4f5801ba46fb
Daniel Leutwyler <daniel.leutwyler@gmx.ch>
2023-11-03 05:57:12 +0100

    ssl connection basics running

--- src/database.ts
+++ src/database.ts
@@ -4,6 +4,7 @@ import { Pool } from 'pg';
 dotenv.config();
 
 const {
+  AWS_RDS_ENDPOINT,
   POSTGRES_DEV_DB,
   POSTGRES_TEST_DB,
   POSTGRES_USER,
@@ -13,29 +14,27 @@ const {
 
 let Client: Pool;
 
-const AWS_RDS_ENDPOINT = 'database-1.ch67ubfskttk.eu-west-3.rds.amazonaws.com';
+const sslOptions = {
+  rejectUnauthorized: false, // Set to true if you want to verify the certificate
+  ca: '../sslcerts/eu-central-1-bundle.pem',
+  // cert: '',
+  // key: '../sslcerts/private-key.pem',
+};
 
-if (ENV?.trim() == 'test') {
-  // trim is used to ensure trailing spaces are removed
-  Client = new Pool({
-    host: AWS_RDS_ENDPOINT, // my aws rds endpoint
-    database: POSTGRES_TEST_DB,
-    user: POSTGRES_USER,
-    password: POSTGRES_PASSWORD,
-  });
-  console.log(
-    `Starting database: ENV=[${ENV}] | database = ${POSTGRES_TEST_DB}`
-  );
-} else {
-  Client = new Pool({
-    host: AWS_RDS_ENDPOINT, // my aws rds endpoint
-    database: POSTGRES_DEV_DB,
-    user: POSTGRES_USER,
-    password: POSTGRES_PASSWORD,
-  });
-  console.log(
-    `Starting database: ENV=[${ENV}] | database = ${POSTGRES_DEV_DB}`
-  );
-}
+const connectionString = `postgres://${POSTGRES_USER}:${POSTGRES_PASSWORD}@${AWS_RDS_ENDPOINT}:5432/${
+  ENV === 'test' ? POSTGRES_TEST_DB : POSTGRES_DEV_DB
+}`;
+
+Client = new Pool({
+  //connectionString: `postgres://postgres:postgres@database-1.chhg8l37c6tv.eu-central-1.rds.amazonaws.com:5432/postgres`, // sslmode=verify-full`,
+  connectionString: connectionString,
+  ssl: sslOptions, // Enable SSL for all environments*/
+});
+
+console.log(
+  `Starting database: ENV=[${ENV}] | database = ${
+    ENV === 'test' ? POSTGRES_TEST_DB : POSTGRES_DEV_DB
+  }  ConnectionString=${connectionString}`
+);
 
 export default Client;
