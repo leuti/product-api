0b18086fb26d6ad1b4a3b2efb81cfaaa5b498559
Daniel Leutwyler <daniel.leutwyler@gmx.ch>
2023-11-18 23:05:35 +0100

    db-migrate changed to call env

--- dist/database.js
+++ dist/database.js
@@ -14,11 +14,19 @@ const sslOptions = {
     // cert: '',
     // key: '../sslcerts/private-key.pem',
 };
-const connectionString = `postgres://${POSTGRES_USER}:${POSTGRES_PASSWORD}@${AWS_RDS_ENDPOINT}:5432/${ENV === 'prod' ? POSTGRES_TEST_DB : POSTGRES_DEV_DB}`;
+let database = ''; // Verwende 'string' anstelle von 'String'
+if (ENV === 'prod') {
+    database = POSTGRES_TEST_DB || ''; // Fallback zu leerem String, falls undefined
+}
+else {
+    database = POSTGRES_DEV_DB || ''; // Fallback zu leerem String, falls undefined
+}
+const connectionString = `postgres://${POSTGRES_USER}:${POSTGRES_PASSWORD}@${AWS_RDS_ENDPOINT}:5432/${database}`;
 Client = new pg_1.Pool({
     //connectionString: `postgres://postgres:postgres@database-1.chhg8l37c6tv.eu-central-1.rds.amazonaws.com:5432/postgres`, // sslmode=verify-full`,
     connectionString: connectionString,
     ssl: sslOptions, // Enable SSL for all environments*/
 });
-console.log(`Starting database: ENV=[${ENV}] | database = ${ENV === 'prod' ? POSTGRES_TEST_DB : POSTGRES_DEV_DB}  ConnectionString=${connectionString}`);
+console.log(`ENV: ${ENV} | `);
+console.log(`Starting database: ENV=[${ENV}] | database = ${database}  ConnectionString=${connectionString}`);
 exports.default = Client;
